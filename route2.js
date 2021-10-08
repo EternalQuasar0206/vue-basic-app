1e0a260f86d601237826e07d50dfb0688f41578b
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-08 15:55:59 -0300

    update routes

--- route2.js
+++ route2.js
@@ -1,5 +1,8 @@
 const route2 = {
     template: (`
         <b>Route 2</b>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis erat nulla. Nulla sit amet urna in nisi varius vehicula vel a mi. Maecenas faucibus volutpat lectus in volutpat. Vivamus ut lectus sit amet neque accumsan fringilla. Donec porttitor magna at nibh feugiat cursus. Aenean mattis mattis nunc, vel dapibus purus condimentum nec. Nulla mollis suscipit vehicula. Maecenas mattis mollis tortor, nec efficitur nisi consequat vitae. Pellentesque pretium venenatis orci, id pulvinar lectus auctor at. Nam dignissim nibh ultricies suscipit pharetra.
+        Curabitur accumsan non erat eu congue. Integer non libero lacinia, tempor ipsum quis, lacinia est. Etiam euismod non nibh gravida ornare. Mauris varius turpis non dolor scelerisque, ac pulvinar augue egestas. Duis id sapien pellentesque, aliquam lorem eget, tempor ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras non dolor dictum, placerat magna vel, tincidunt nunc. Phasellus nibh justo, malesuada eget viverra nec, tristique in diam.
     `)
 }
